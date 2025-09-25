d0f2a3c58b018777d348d7ce0994c459a52464a8
peter_mrz <peter.mraz@kosickaakademia.sk>
2025-09-25 10:04:24 +0200

    compiled demo

---
+++ include/lv_conf.h
@@ -0,0 +1,41 @@
+/*
+ * lv_conf.h - LVGL configuration file
+ * You can customize LVGL features here. For more options, see LVGL documentation.
+ */
+
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#define LV_COLOR_DEPTH           16
+#define LV_USE_LOG               1
+#define LV_USE_LABEL             1
+#define LV_USE_BTN               1
+#define LV_USE_IMG               1
+#define LV_USE_LINE              1
+#define LV_USE_ARC               1
+#define LV_USE_BAR               1
+#define LV_USE_SLIDER            1
+#define LV_USE_SWITCH            1
+#define LV_USE_LED               1
+#define LV_USE_MSGBOX            1
+#define LV_USE_TEXTAREA          1
+#define LV_USE_CANVAS            1
+#define LV_USE_CHART             1
+#define LV_USE_TABLE             1
+#define LV_USE_TILEVIEW          1
+#define LV_USE_WIN               1
+#define LV_USE_SPINBOX           1
+#define LV_USE_LIST              1
+#define LV_USE_DROPDOWN          1
+#define LV_USE_ROLLER            1
+#define LV_USE_TABVIEW           1
+#define LV_USE_SPINNER           1
+#define LV_USE_CALENDAR          1
+#define LV_USE_KEYBOARD          1
+#define LV_USE_ANIMATION         1
+#define LV_USE_THEME_DEFAULT     1
+#define LV_USE_THEME_BASIC       1
+#define LV_USE_THEME_MATERIAL    1
+#define LV_USE_THEME_MONO        1
+
+#endif /* LV_CONF_H */
