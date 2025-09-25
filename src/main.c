4e072dc515842ca04dbe0e11fe3aa725493e54ad
Penky777 <lukas.penkert@gmail.com>
2025-09-25 09:52:08 +0200

    Testing screen

--- src/main.c
+++ src/main.c
@@ -1,3 +1,19 @@
-void app_main() {
-    
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define LED_GPIO 8  // Change if your board's LED is on a different pin
+
+void app_main(void)
+{
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(LED_GPIO, 1);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        gpio_set_level(LED_GPIO, 0);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
 }
